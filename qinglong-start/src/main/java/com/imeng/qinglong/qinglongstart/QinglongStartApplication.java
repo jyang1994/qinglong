4fcc818aeece168746dd57d882bc19b09425d7e4
杨梦龙 <liudao.yang@tuya.com>
2021-10-09 13:58:13 +0800

    项目初始化

---
+++ qinglong-start/src/main/java/com/imeng/qinglong/qinglongstart/QinglongStartApplication.java
@@ -0,0 +1,13 @@
+package com.imeng.qinglong.qinglongstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QinglongStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QinglongStartApplication.class, args);
+    }
+
+}
